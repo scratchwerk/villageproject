da44f400982ac1f9dea7ab6dc479fdce48f50fb8
kingr <ronnie.king@gmail.com>
2013-07-29 12:59:50 -0400

    something

--- VillageProject/src/com/scratchwerk/villageproject/VillageProjectServlet.java
+++ VillageProject/src/com/scratchwerk/villageproject/VillageProjectServlet.java
@@ -7,5 +7,6 @@ public class VillageProjectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		//fsdfsfsd
 	}
 }
