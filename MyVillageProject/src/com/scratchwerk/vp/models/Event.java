e327bc25df8bfd9949b26686c4b6b58295849f5e
scratchwerk <ronnie.king@gmail.com>
2013-08-13 07:48:52 -0400

    Event model change

--- MyVillageProject/src/com/scratchwerk/vp/models/Event.java
+++ MyVillageProject/src/com/scratchwerk/vp/models/Event.java
@@ -7,12 +7,16 @@ public class Event {
 	private int id;
 	private String name;
 	private Date date;
-	private int orgID;
+	private Organization organizaer;
 	private String location;
 	private String address;
 	private String city;
 	private String state;
 	private int zip;
+	
+	private boolean isOneTime = false;
+	private boolean isClub = false;
+	private boolean isSupport = false;
 	
 	
 }
